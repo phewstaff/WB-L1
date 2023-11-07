6411e8df3b1123762206f0bfe7cccc475428682d
magomed_n <naurbaev2020@gmail.com>
2023-11-07 23:55:51 +0300

    checking for perfect number

---
+++ 2.checkIsPerfectNumber.js
@@ -0,0 +1,29 @@
+/*  Задача о странных числах: Напишите функцию, 
+которая принимает число и возвращает true, если это число является странным,
+и false в противном случае. Странным числом считается число,
+которое равно сумме всех своих делителей, кроме самого себя.
+ */
+
+// в описании говорится о совершенном числе, потому что странное число- наоборот, число у котороого из суммы делителей никогда не получится то же самое число.
+
+const checkIsWeirdNumber = (number) => {
+  dividers = [];
+  sum = 0;
+  for (let i = 0; i <= number; i++) {
+    number % i === 0 ? dividers.push(i) : "";
+  }
+
+  dividers.forEach((element) => {
+    sum += element;
+  });
+
+  if (sum / number === 2) {
+    return true;
+  } else return false;
+};
+
+console.log(checkIsWeirdNumber(6)); // true
+console.log(checkIsWeirdNumber(10)); // false
+console.log(checkIsWeirdNumber(28)); // true
+console.log(checkIsWeirdNumber(496)); // true
+console.log(checkIsWeirdNumber(8128)); //  true
