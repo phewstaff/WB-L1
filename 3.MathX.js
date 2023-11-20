8d18a6c8cb93848a4aef208c71f84fa7a80edb1a
magomed_n <naurbaev2020@gmail.com>
2023-11-20 14:31:23 +0300

    задание 3

---
+++ 3.MathX.js
@@ -0,0 +1,96 @@
+/* Последовательность чисел Фибоначчи определяется формулой Fn = Fn-1 + Fn-2. То есть, следующее число получается как сумма двух предыдущих.
+
+Первые два числа равны 1, затем 2(1+1), затем 3(1+2), 5(2+3) и так далее: 1, 1, 2, 3, 5, 8, 13, 21.... */
+
+const MathX = {
+  findNthFibonacci: (n, sequence = [1, 1]) => {
+    if (n < 2) {
+      return 1; // первые два элемента в данном примере нужно обозначать вруччную, так как первый элемент выдаст undefined
+    }
+
+    const lastInSequence = sequence[sequence.length - 1]; // обозначаем последний элеиент в списке
+    const preLastInSequence = sequence[sequence.length - 2]; // предпоследний элемент
+
+    if (sequence.length === n) {
+      return sequence[sequence.length - 1]; // если реурсия дошла до n-ого числа, возвращаем результат
+    }
+
+    sequence.push(preLastInSequence + lastInSequence); // иначе добавляем следующее число фибоначчи в массив
+
+    return MathX.findNthFibonacci(n, sequence); // вызываем рекурсию.
+  },
+
+  FibSequence: (n, sequence = [1, 1]) => {
+    if (n < 2) {
+      return 1;
+    }
+
+    const lastInSequence = sequence[sequence.length - 1];
+    const preLastInSequence = sequence[sequence.length - 2];
+
+    if (sequence.length === n) {
+      return sequence;
+    }
+
+    sequence.push(preLastInSequence + lastInSequence); // практически то же самое, что и в первом методе, только возвращаем сам массив
+
+    return MathX.FibSequence(n, sequence);
+  },
+
+  findNthPrime: (function () {
+    // Helper функция для проверки является ли число простым.
+    function isPrime(num) {
+      for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+          return false; // возвращаем false, если число делится на другие числа, кроме себя,
+        }
+      }
+      return num > 1; // 1 делится на себя и только на себя, поэтому исключаем вариант
+    }
+
+    return function (n) {
+      const primes = [];
+      let currentNum = 2; // также начинаем с 1
+
+      while (primes.length < n) {
+        if (isPrime(currentNum)) {
+          primes.push(currentNum); // проходимся по массиву и добавляем туда простые числа, после проверки
+        }
+        currentNum++; // итерация для цилка
+      }
+
+      return primes[n - 1]; // возвращаем последний элемент, который является искомым, после остановки цикла
+    };
+  })(),
+
+  findPrimesUpTo: (function () {
+    function isPrime(num) {
+      for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+          return false;
+        }
+      }
+      return num > 1;
+    }
+
+    return function (n) {
+      const primes = [];
+      let currentNum = 2;
+
+      while (primes.length < n) {
+        if (isPrime(currentNum)) {
+          primes.push(currentNum);
+        }
+        currentNum++;
+      }
+
+      return primes; // все то же самое, возвращаем сам массив
+    };
+  })(),
+};
+
+// примеры
+console.log(MathX.findNthFibonacci(7)); // 13
+console.log(MathX.FibSequence(9)); // [1, 1, 2, 3, 5, 8, 13, 21, 34]
+console.log(MathX.findNthPrime(5)); // 11
+console.log(MathX.findPrimesUpTo(5)); //  [ 2, 3, 5, 7, 11 ]
